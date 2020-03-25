0afd6cc1147ef533be9e82d8e1f38f2851aabae2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-25 07:05:32 +0000

    Deploy savaleks/Covid19 to github.com/savaleks/Covid19.git:gh-pages

---
+++ precache-manifest.057affb2fc4d7f8971b97c02dd5eb464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98ed7fbd05fbc2d0f6ffe7a5397c5b2",
+    "url": "/Covid19/index.html"
+  },
+  {
+    "revision": "f5a680419bbc7f64ad14",
+    "url": "/Covid19/static/css/main.691ac075.chunk.css"
+  },
+  {
+    "revision": "451b54f04b0e2a94d128",
+    "url": "/Covid19/static/js/2.79750929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid19/static/js/2.79750929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a680419bbc7f64ad14",
+    "url": "/Covid19/static/js/main.ba767221.chunk.js"
+  },
+  {
+    "revision": "99d77cc76e8e62a6b442",
+    "url": "/Covid19/static/js/runtime-main.3c383c68.js"
+  }
+]);
